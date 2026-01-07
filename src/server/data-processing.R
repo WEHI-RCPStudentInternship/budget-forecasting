94abb10fcd3e157711bc51908b2c86a270834240
KhanhDo <khanhanbibo@gmail.com>
2026-01-07 15:42:03 +1100

    initial commit: data processing

--- src/server/data-processing.R
+++ src/server/data-processing.R
@@ -1,19 +1,69 @@
 
 
+# Return: List of the expense and funding data from the excel file
+read_excel_data <- function(file_path)
+{
+  funding_sources_df <- read_excel(file_path, sheet = "Funding") %>%
+    process_funding_data()
+
+  expense_df <- read_excel(file_path, sheet = "Expense") %>%
+    process_expense_data()
+
+  return(list(funding_sources = funding_sources_df, expense = expense_df))
+}
 # Handles data processing
 
 
-process_funding_data <- function(file_path) {
-  
-  # file_path: string of the spreadsheet's filepath
-  
+process_funding_data <- function(df) {
+  # Read the dataframe, select and rename columns for the funding data
+  # df: data frame read from the excel sheet
+  # Returns:
+  # funding_sources_df: processed funding sources data frame
+
+  funding_sources_df <- df %>%
+    select(`Source ID`, `Allowed Categories`, `Valid From`, `Valid To`, `Amount`) %>%
+    setNames(nm = c("source_id", "allowed_categories", "valid_from", "valid_to", "amount")) %>%
+
+    # Convert data types
+    mutate(
+      valid_from = as.Date(valid_from),
+      valid_to = as.Date(valid_to),
+      amount = as.numeric(amount),
+      source_id = as.character(source_id)
+    ) %>%
+
+    # Unnest categories
+    mutate(
+      allowed_categories = strsplit(as.character(allowed_categories), ",\\s*")
+    ) %>%
+    rowwise() %>%
+    mutate(allowed_categories = list(allowed_categories)) %>%
+    unnest(allowed_categories) %>%
+
+    # Remove rows with NA in source_id
+    filter(!is.na(source_id))
+
+  return(funding_sources_df)
 }
 
-process_expense_data <- function(file_path) {
-  
-  # file_path: string of the spreadsheet's filepath
-  
-  
-}
+process_expense_data <- function(df) {
+  # Read the dataframe, select and rename columns for the expense data
+  #
+  # Arguments:
+  # df: data frame read from the excel sheet
+  # Returns:
+  # expense_df: processed expense data frame
 
-# Helper functions:
+  expense_df <- df %>%
+    select(`Item ID`, `Expense Category`, `Planned Amount`, `Latest Payment Date`) %>%
+    setNames(nm = c("item_id", "expense_category", "planned_amount", "latest_payment_date")) %>%
+    
+    # Convert data types
+    mutate(
+      latest_payment_date = as.Date(latest_payment_date),
+      planned_amount = as.numeric(planned_amount),
+      item_id = as.character(item_id)
+    )
+
+  return(expense_df)
+}
